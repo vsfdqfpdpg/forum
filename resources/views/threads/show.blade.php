c21b696ac01e1460ca8efc5ec36dad102f1bde80
vsfdqfpdpg <zoujian49@126.com>
2018-02-27 16:58:58 +0800

    Lesson 05

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,5 +23,20 @@
                 @endforeach
             </div>
         </div>
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ $thread->path().'/replies' }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" cols="30" rows="5" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
